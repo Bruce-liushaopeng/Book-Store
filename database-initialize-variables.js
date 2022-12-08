6309296f7e6472ce051d39e46ff9a34a4c340adc
bruce <bruceliu1107@gmail.com>
2022-12-07 22:18:44 -0500

    querytrigger initalizer update

--- database-initialize-variables.js
+++ database-initialize-variables.js
@@ -449,20 +449,23 @@ END;
 $$ LANGUAGE plpgsql;
 `;
 
-exports.initialTriggerQuery = `CREATE or replace FUNCTION book_stock_check() RETURNS trigger AS $book_stock_check$
-BEGIN
-    -- checking book stock quatities, if less than 10, auto plus 15
-    IF NEW.quantityInStock < 10 THEN
-        -- auto ordering and stock up
-        update book set quantityInStock = NEW.quantityInStock + (select SellsAmount from lastMonthSell where lastMonthSell.ISBN = book.ISBN) 
-        where book.ISBN = NEW.ISBN;
-    END IF;
-    RETURN NEW;
-END;
+exports.initialTriggerQuery = `-- trigger function when update book stock amount
+CREATE or replace FUNCTION book_stock_check() RETURNS trigger AS $book_stock_check$
+    BEGIN
+        -- checking book stock quatities, if less than 10, auto plus 15
+        IF NEW.quantityInStock < 10 THEN
+            -- auto ordering and stock up
+            update book set quantityInStock = NEW.quantityInStock + (select SellsAmount from lastMonthSell where lastMonthSell.ISBN = book.ISBN) 
+            where book.ISBN = NEW.ISBN;
+        END IF;
+        RETURN NEW;
+    END;
 $book_stock_check$ LANGUAGE plpgsql;
 
-CREATE or replace TRIGGER book_stock_check AFTER UPDATE ON Book	
-FOR EACH ROW EXECUTE FUNCTION book_stock_check();`	
+-- Trigger to listen on the update action on Book and invoke book_stock_check trigger function.
+CREATE or replace TRIGGER book_stock_check AFTER UPDATE ON Book
+    FOR EACH ROW EXECUTE FUNCTION book_stock_check();
+`	
 
 
 exports.getAllBooks = `select * from book`
